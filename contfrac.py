ac3debb260f5a86f8b184e9be037a8cbd59d739f
Matjaž <dev@matjaz.it>
2019-04-12 19:14:52 +0200

    Implement computation of a continued fraction
    - Rename value_finite() to evaluate()
    - Add some examples in the docstrings

--- contfrac.py
+++ contfrac.py
@@ -1,15 +1,101 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import fractions
 import typing
 
 
-def value_finite(cont_frac):
+def continued_fraction(x, maxlen=30):
+    """Computes the continued fraction of a number ``x`` expressed in many
+    types, including floats, tuples and Fractions.
+
+    The result for a number ``x`` will be the list of coefficients
+    ``[a, b, c, d, ...]```:
+
+                           1
+            x = a + ---------------
+                             1
+                    b + -----------
+                               1
+                        c + -------
+                            d + ...
+
+
+    Warning:
+        If ``x`` is a float, then rounding errors may occur and the continued
+        fraction is not guaranteed to look exactly as computed by hand,
+        although if its value is evaluated with ``evaluate()``, it may be
+        close-enough to ``x``.
+
+        To avoid such issues, consider providing ``x`` as a ratio of integers,
+        either as a tuple (numerator, denominator), which can be obtained from
+        a float with its method ``as_integer_ratio()``, or as Fraction.
+        Be aware that in this case the continued fraction may look
+        different than computed by hand but its value once evaluated will
+        be exactly the same as the input ratio.
+
+        Given the finite precision a floating point value has, even an
+        irrational number such as pi or e will be correctly represented
+        up to a certain point in the continued fraction. For example the golden
+        ratio will contain a ``2`` at index 38 of the continued fraction,
+        when it should be ``1``.
+
+    Args:
+        x (Union[Tuple[int,int], float, int, fractions.Fraction]): the value
+            to compute the continued fraction of.
+        maxlen (int): upper limit to the length of the produced list,
+            especially useful when computing continued fractions of irrational
+            numbers.
+
+    Returns:
+        List[int]: continued fraction
+    """
+    if maxlen <= 0:
+        raise ValueError('maxlen must be positive.')
+    elif isinstance(x, int):
+        return list(__int_cont_frac(x, 1, max_amount=1))
+    elif isinstance(x, float):
+        return list(__float_cont_frac(x, maxlen))
+    elif isinstance(x, fractions.Fraction):
+        return list(__int_cont_frac(x.numerator, x.denominator, maxlen))
+    elif isinstance(x, (tuple, list)):
+        return list(__int_cont_frac(x[0], x[1], maxlen))
+    else:
+        raise TypeError('Unsupported input type {:}'.format(type(x)))
+
+
+def __int_cont_frac(num, den, max_amount):
+    amount = 0
+    while den != 0 and amount < max_amount:
+        integer_part = num // den
+        num -= integer_part * den
+        num, den = den, num
+        amount += 1
+        yield integer_part
+
+
+def __float_cont_frac(real_number, max_amount):
+    fractional_part = 42
+    amount = 0
+    abs_tol = 10**-10
+    while not abs(fractional_part) <= abs_tol and amount < max_amount:
+        integer_part = int(round(real_number, 10))
+        fractional_part = real_number - integer_part
+        real_number = 1.0 / fractional_part
+        amount += 1
+        yield integer_part
+
+
+def evaluate(cont_frac):
     """Computes the floating point value of a finite continued fraction
     representation.
 
-    That is the value of `c[0] + 1/(c[1] + 1/(c[2] + 1/(c[3] + ...)))`
-    for an input `c`.
+    That is the value of ``c[0] + 1/(c[1] + 1/(c[2] + 1/(c[3] + ...)))``
+    for an input ``c``.
+
+    Example:
+        For an input of ``[2,3,4]`` is ``2 + 1/(3 + 1/4) = 30/13`` expressed as
+        2.3076923076923075.
 
     Args:
         cont_frac (Iterable[Union[int, float]]): representation of a continued
@@ -34,6 +120,9 @@ def arithmetical_expr(cont_frac, with_spaces=True, force_floats=False):
     The string is ready to be evaluated by a functions like `eval()` or
     by other programming languages or calculators. Beware of integer division
     instead of true division in said language: use the `force_floats` argument.
+
+    Example:
+        It looks like ``2 + 1/(3 + 1/(4 + 1/(5)))`` for an input ``[2,3,4,5]``.
 
     Args:
         cont_frac (Iterable[Union[int, float]]): representation of a continued
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import fractions
+import math
 import unittest
 
 import contfrac
@@ -31,7 +33,7 @@ class TestEvaluateContfrac(unittest.TestCase):
         }
         for input_value, expected_output in test_values.items():
             with self.subTest(contfrac_of=input_value):
-                result = contfrac.value_finite(input_value)
+                result = contfrac.evaluate(input_value)
                 self.assertAlmostEqual(expected_output, result, places=6)
 
     def test_evaluate_continued_fraction_with_zero_end(self):
@@ -44,13 +46,13 @@ class TestEvaluateContfrac(unittest.TestCase):
         for value in values:
             with self.subTest(input_value=value):
                 with self.assertRaises(ZeroDivisionError):
-                    contfrac.value_finite(value)
+                    contfrac.evaluate(value)
 
     def test_evaluate_continued_fraction_unsupported_type_raises(self):
-        self.assertRaises(TypeError, contfrac.value_finite, None)
-        self.assertRaises(TypeError, contfrac.value_finite, 1)
-        self.assertRaises(TypeError, contfrac.value_finite, 1.1)
-        self.assertRaises(TypeError, contfrac.value_finite, 'hello')
+        self.assertRaises(TypeError, contfrac.evaluate, None)
+        self.assertRaises(TypeError, contfrac.evaluate, 1)
+        self.assertRaises(TypeError, contfrac.evaluate, 1.1)
+        self.assertRaises(TypeError, contfrac.evaluate, 'hello')
 
 
 class TestArithmeticExpression(unittest.TestCase):
@@ -97,3 +99,79 @@ class TestArithmeticExpression(unittest.TestCase):
                 expected_output = expected_default.replace(' ', '')
                 expected_output = expected_output.replace('1/', '1.0/')
                 self.assertEqual(expected_output, result)
+
+
+class TestContfracComputation(unittest.TestCase):
+    def test_continued_fraction_legal_values(self):
+        test_values = {
+            # Integers
+            0: [0],
+            1: [1],
+            123: [123],
+            -1: [-1],
+            -123: [-123],
+
+            # Tuples: (nominator, denominator)
+            (649, 200): [3, 4, 12, 4],
+            (415, 93): [4, 2, 6, 7],
+            (-649, 200): [-4, 1, 3, 12, 4],
+            (415, -93): [-5, 1, 1, 6, 7],
+
+            # Fractions
+            fractions.Fraction(649, 200): [3, 4, 12, 4],
+            fractions.Fraction(415, 93): [4, 2, 6, 7],
+            fractions.Fraction(-649, 200): [-4, 1, 3, 12, 4],
+            fractions.Fraction(415, -93): [-5, 1, 1, 6, 7],
+
+            # Floats
+            649 / 200: [3, 4, 12, 4],
+            -649 / 200: [-3, -4, -12, -4],
+            415 / 93: [4, 2, 6, 7],
+            0.84375: [0, 1, 5, 2, 2],
+        }
+        for input_value, expected_output in test_values.items():
+            with self.subTest(contfrac_of=input_value):
+                result = contfrac.continued_fraction(input_value)
+                self.assertListEqual(expected_output, result)
+                with self.subTest(evaluating_contfrac_of=input_value):
+                    if isinstance(input_value, tuple):
+                        input_value = input_value[0] / input_value[1]
+                    elif isinstance(input_value, fractions.Fraction):
+                        input_value = float(input_value)
+                    evaluated = contfrac.evaluate(result)
+                    self.assertAlmostEqual(input_value, evaluated, delta=1e-8)
+
+    def test_continued_fraction_illegal_maxlen(self):
+        self.assertRaises(ValueError, contfrac.continued_fraction, 2.2,
+                          maxlen=-1)
+        self.assertRaises(ValueError, contfrac.continued_fraction, 2.2,
+                          maxlen=0)
+        contfrac.continued_fraction(2.2, maxlen=1)
+
+    def test_continued_fraction_unsupported_type_raises(self):
+        self.assertRaises(TypeError, contfrac.continued_fraction, None)
+        self.assertRaises(TypeError, contfrac.continued_fraction, 'hello')
+        self.assertRaises(TypeError, contfrac.continued_fraction, b'hello')
+        self.assertRaises(TypeError, contfrac.continued_fraction, dict())
+
+    def test_continued_fraction_golden_ratio(self):
+        golden_ratio = (1 + math.sqrt(5)) / 2
+        expected = [1] * 40
+        result = contfrac.continued_fraction(golden_ratio, maxlen=2)
+        self.assertListEqual(expected[:2], result)
+        result = contfrac.continued_fraction(golden_ratio, maxlen=20)
+        self.assertListEqual(expected[:20], result)
+        result = contfrac.continued_fraction(golden_ratio, maxlen=31)
+        self.assertListEqual(expected[:31], result)
+
+    def test_rounding_errors(self):
+        golden_ratio = (1 + math.sqrt(5)) / 2
+        as_ratio = golden_ratio.as_integer_ratio()
+        result = contfrac.continued_fraction(golden_ratio, maxlen=50)
+        self.assertNotEqual([1] * 50, result)
+        evaluated_value = contfrac.evaluate(result)
+        self.assertEqual(golden_ratio, evaluated_value)
+        result = contfrac.continued_fraction(as_ratio, maxlen=50)
+        self.assertNotEqual([1] * 50, result)
+        evaluated_value = contfrac.evaluate(result)
+        self.assertEqual(as_ratio, evaluated_value.as_integer_ratio())
