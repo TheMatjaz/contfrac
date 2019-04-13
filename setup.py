09ec8f1794576bc2c2f5d90674f90fd405127622
Matjaž <dev@matjaz.it>
2019-04-13 15:44:00 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Package setup for the contfrac library."""
+
+from distutils.core import setup
+
+setup(
+    name='contfrac',
+    version='1.0.0',
+    description='Continued fractions library',
+    long_description=open('README.md').read(),
+    author='Matjaž Guštin',
+    author_email='dev@matjaz.it',
+    url='https://github.com/TheMatjaz/contfrac',
+    py_modules=['contfrac'],
+    keywords=['continued', 'fraction', 'fractions', 'convergent', 'rational'],
+    classifiers=[
+        'Development Status :: Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires='>=3.4, <4',
+)
